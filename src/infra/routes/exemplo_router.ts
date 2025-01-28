65d6bc9c370a8564a5a23fd697432cdfff32d652
Fernando Carvalho <fernando.carvalho.casa@gmail.com>
2025-01-28 08:39:22 -0300

    router mínimo (o vivo não deveria estar aqui)

--- src/infra/routes/exemplo_router.ts
+++ src/infra/routes/exemplo_router.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { factoryControllerBasico } from "../factories/factory-controller-basico";
 
-const router = Router();
+const routerBasico = Router();
 
 const objeto = factoryControllerBasico();
 
-router.get('/exemplo', (req, res) => {
+routerBasico.get('/vivo', (req, res) => {
+  res.status(200).json({ message: 'Estou vivo!' });
+});
+
+routerBasico.get('/basico/:valor', (req, res) => {
   objeto.handle(req, res);
 });
+
+export { routerBasico };
