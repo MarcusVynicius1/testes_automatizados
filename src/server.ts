598bfa9219d85b67ffce5c5dc87d709c0b8331cf
Fernando Carvalho <fernando.carvalho.casa@gmail.com>
2025-01-28 08:32:34 -0300

    server básico mínimo

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import router from './infra/routes/exemplo_router';
+import { routerBasico } from './infra/routes/exemplo_router';
 
 const app = express();
 
+app.use(express.json());
+app.use(routerBasico);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
