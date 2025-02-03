eaa849f25d68b442bd0a859bdef62259f1b43ae3
MarcusVynicius1 <91838315+MarcusVynicius1@users.noreply.github.com>
2025-02-02 22:09:35 -0300

    Realizada pequenas alterações no código.

--- src/infra/routes/nota-routes.ts
+++ src/infra/routes/nota-routes.ts
@@ -4,7 +4,7 @@ import { ConsultarNotas } from "../../domain/usecases/consultar-notas";
 import { NotaRepositoryImpl } from "../../data/repositories/nota-repository-impl";
 
 const router = express.Router();
-const notaRepository = new NotaRepositoryImpl(); // Use a implementação concreta
+const notaRepository = new NotaRepositoryImpl();
 const consultarNotas = new ConsultarNotas(notaRepository);
 const notaController = new NotaController(consultarNotas);
 
